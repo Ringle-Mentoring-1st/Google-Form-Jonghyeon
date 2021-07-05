05d67fcdaa32759178ecede1d84627a72589ce07
Jonghyeon Ko <flowithusdev@gmail.com>
2021-07-06 00:49:00 +0900

    Update - NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,40 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-// Assets
-import Logo from '../assets/logo512.png';
+import Container from '../ui/Container';
 // Components
 import LogoutButton from './LogoutButton';
 
-function NavBar() {
+interface NavBarProps {
+  logoSrc: string;
+}
+
+function NavBar({ logoSrc }: NavBarProps) {
   return (
-    <nav>
-      <Link aria-label="logo" to="/form/list">
-        <img src={Logo} alt="logo" />
-      </Link>
-      <ul>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <Container>
+        <nav
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            height: 60,
+            padding: 16,
+          }}
+        >
+          <Link aria-label="logo" to="/form/list">
+            <img src={logoSrc} alt="logo" style={{ height: '100%' }} />
+          </Link>
+          <ul
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <li>
+              <LogoutButton />
+            </li>
+          </ul>
+        </nav>
+      </Container>
+    </div>
   );
 }
 
