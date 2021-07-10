aa3e41130e7cd643e6dbab75ad144a8fd6816a5b
Jonghyeon Ko <flowithusdev@gmail.com>
2021-07-11 02:09:37 +0900

    Style - ChoiceInput Styling beautifully

--- src/components/ChoiceInput.tsx
+++ src/components/ChoiceInput.tsx
@@ -29,21 +29,44 @@ function ChoiceInput({
 
   return (
     <ChoiceInputContainer>
-      <input
-        id={id}
-        type={choiceType}
-        checked={isSelected}
-        onChange={onChangeCheck}
-      />
-      {!isCompleted ? (
-        <TextInput
-          value={value}
-          placeholder={placeholder}
-          fill
-          onChange={onChangeTextInput}
-        />
-      ) : (
-        <text>{value}</text>
+      {isCompleted && (
+        <Styled onClick={onChangeCheck} type={choiceType} checked={isSelected}>
+          <div className="choice-container" id={id}>
+            <div className="choice">
+              {choiceType === 'checkbox' && (
+                <Icon.Check
+                  className="check"
+                  style={{ color: 'white' }}
+                ></Icon.Check>
+              )}{' '}
+            </div>
+          </div>
+
+          <label htmlFor={id}>{value}</label>
+        </Styled>
+      )}
+
+      {!isCompleted && (
+        <>
+          <Styled type={choiceType} checked={false}>
+            <div className="choice-container" id={id}>
+              <div className="choice">
+                {choiceType === 'checkbox' && (
+                  <Icon.Check
+                    className="check"
+                    style={{ color: 'white' }}
+                  ></Icon.Check>
+                )}{' '}
+              </div>
+            </div>
+            <TextInput
+              value={value}
+              placeholder={placeholder}
+              fill
+              onChange={onChangeTextInput}
+            />
+          </Styled>
+        </>
       )}
       {!isCompleted && (
         <Button
@@ -64,4 +87,83 @@ const ChoiceInputContainer = styled.div`
   margin-top: 6px;
   display: flex;
   align-items: center;
+  margin-top: 12px;
+  position: relative;
+`;
+
+const Styled = styled.div<{ type: string; checked: boolean }>`
+  flex: 1;
+  font-weight: 700;
+  background: ${({ theme }) => theme.colors.gray100};
+  display: flex;
+  align-items: center;
+  padding: 12px;
+  border-radius: 16px;
+  &:hover {
+    background: ${({ theme }) => theme.colors.gray200};
+  }
+  transition: all 0.2s ease-in-out;
+
+  > .choice-container {
+    z-index: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 20px;
+    height: 20px;
+    background: ${({ theme }) => theme.colors.white};
+    border-radius: ${({ type }) => (type === 'checkbox' ? '6px' : '999px')};
+    overflow: hidden;
+    box-shadow: 0px 0px 0px 0px ${({ theme }) => theme.colors.gray300},
+      inset 0px 0px 0px
+        ${({ checked, theme, type }) =>
+          type === 'checkbox'
+            ? (checked ? '32px' : '0px') +
+              (checked ? theme.colors.primary : theme.colors.gray300)
+            : 0 + (checked ? theme.colors.white : theme.colors.gray300)};
+    border: 3px solid
+      ${({ theme, checked }) =>
+        checked ? theme.colors.primary : theme.colors.gray300};
+    transition: all 0.2s ease-in-out;
+    margin-right: 6px;
+
+    &:hover {
+      box-shadow: 0px 0px 0px 0px
+          ${({ theme, checked }) =>
+            checked ? theme.colors.white : theme.colors.gray300},
+        inset 0px 0px 0px 32px
+          ${({ theme, checked }) =>
+            checked ? theme.colors.white : theme.colors.gray300};
+      transition: all 0.04s ease-in-out;
+    }
+
+    > .choice {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: ${({ checked, type }) => {
+        if (type === 'checkbox') {
+          return checked === true ? '90px' : '0px';
+        } else if (type === 'radio') {
+          return checked === true ? '14px' : '0px';
+        }
+      }};
+      height: ${({ checked, type }) => {
+        if (type === 'checkbox') {
+          return checked === true ? '90px' : '0px';
+        } else if (type === 'radio') {
+          return checked === true ? '14px' : '0px';
+        }
+      }};
+      border-radius: ${({ type }) => {
+        if (type === 'checkbox') {
+          return '0px';
+        } else if (type === 'radio') {
+          return '999px';
+        }
+      }};
+      background: ${({ theme }) => theme.colors.primary};
+      transition: all 0.2s ease-in-out;
+    }
+  }
 `;
